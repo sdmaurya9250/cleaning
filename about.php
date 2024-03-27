b02952c33ecae78127ce6142ea48c8d658bca9c9
surendra <surendra75maurya@gmail.com>
2024-03-27 19:00:14 +0530

    all done

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <?php
 require_once('header.php');
 ?>
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/about2.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text align-items-end">
